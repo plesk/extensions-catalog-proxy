b3829f035cea37ed3fd3d70141d03bc3d6f1ecec
Alexei Yuzhakov <sibprogrammer@gmail.com>
2017-02-17 20:47:14 +0700

    Modify only list of extensions.

--- site/index.php
+++ site/index.php
@@ -2,14 +2,16 @@
 
 $data = json_decode(file_get_contents('https://ext.plesk.com' . $_SERVER['PATH_INFO']));
 
-$data = array_filter($data, function($item) {
-    // filter everything sold via go.plesk.com, except docker and wp-toolkit
-    if (0 === strpos($item->buy_url, 'https://go.plesk.com') && !in_array($item->code, ['docker', 'wp-toolkit'])) {
-        return null;
-    }
+if (preg_match('|^/api/v\d+/packages(\?.*)?$|', $_SERVER['PATH_INFO'])) {
+    $data = array_values(array_filter($data, function($item) {
+        // filter everything sold via go.plesk.com, except docker and wp-toolkit
+        if (0 === strpos($item->buy_url, 'https://go.plesk.com') && !in_array($item->code, ['docker', 'wp-toolkit'])) {
+            return null;
+        }
 
-    return $item;
-});
+        return $item;
+    }));
+}
 
 header('Content-Type: application/json');
-echo json_encode(array_values($data));
+echo json_encode($data);
