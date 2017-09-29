d47880b75a9aa88eb5fbc0328effef1c89b38794
mlapkin <mlapkin@users.noreply.github.com>
2017-09-29 18:04:35 +0700

    Remove debug output, improve code

--- site/index.php
+++ site/index.php
@@ -8,13 +8,12 @@ $filterCallback = function ($item) use ($whiteList) {
         : $item;
 };
 
-$data = json_decode(file_get_contents('https://ext.plesk.com' . $_SERVER['PATH_INFO'] . (!empty($_SERVER['QUERY_STRING']) ? '?' . $_SERVER['QUERY_STRING'] : '')));
+$url = 'https://ext.plesk.com' . $_SERVER['PATH_INFO'] . (!empty($_SERVER['QUERY_STRING']) ? '?' . $_SERVER['QUERY_STRING'] : '');
+$data = json_decode(file_get_contents($url));
 
 if (preg_match('|^/api/v\d+/packages(\?.*)?$|', $_SERVER['PATH_INFO'])) {
     $data = array_values(array_filter($data, $filterCallback));
-}
-
-if (preg_match('|^/api/v\d+/categories(\?.*)?$|', $_SERVER['PATH_INFO'])) {
+} elseif (preg_match('|^/api/v\d+/categories(\?.*)?$|', $_SERVER['PATH_INFO'])) {
     $packagesUrl = str_replace('categories', 'packages', $_SERVER['PATH_INFO']);
     $packagesWhitelist = array_map(
         function ($item) {
@@ -22,7 +21,6 @@ if (preg_match('|^/api/v\d+/categories(\?.*)?$|', $_SERVER['PATH_INFO'])) {
         },
         array_filter((array)json_decode(file_get_contents($packagesUrl)), $filterCallback)
     );
-    echo print_r($packagesWhitelist, 1);
     foreach ($data as $category) {
         if (!empty($category->top_packages)) {
             $category->top_packages = array_values(array_filter($category->top_packages, function ($item) use ($packagesWhitelist) {
